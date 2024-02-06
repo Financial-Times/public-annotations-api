6f41b6997dc63c5c5f3ecb37d897f880815e06eb
Anna Grancharova <anna.grancharova@ft.com>
2024-02-06 17:12:02 +0200

    Added: hasReference predicate

--- annotations/models.go
+++ annotations/models.go
@@ -31,6 +31,7 @@ var predicates = map[string]string{
 	"HAS_AUTHOR":                 "http://www.ft.com/ontology/annotation/hasAuthor",
 	"HAS_CONTRIBUTOR":            "http://www.ft.com/ontology/hasContributor",
 	"HAS_DISPLAY_TAG":            "http://www.ft.com/ontology/hasDisplayTag",
+	"HAS_REFERENCE":              "http://www.ft.com/ontology/hasReference",
 	"IS_CLASSIFIED_BY":           "http://www.ft.com/ontology/classification/isClassifiedBy",
 	"HAS_BRAND":                  "http://www.ft.com/ontology/classification/isClassifiedBy",
 	"IS_PRIMARILY_CLASSIFIED_BY": "http://www.ft.com/ontology/classification/isPrimarilyClassifiedBy",
